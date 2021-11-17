22c490877945c62196ba6bcb4a82988c7ef0e7d5
vita ora <vitata710@gmail.com>
2021-11-18 01:01:25 +0800

    apply home page and css style

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.scss';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <section className="home-page">
+      <h5 className="home-page__title">YouBike</h5>
+      <div className="home-page__link">
+        <Link to="/find-bikes" className="home-page__link-item">
+          <span>找 YouBike</span>
+        </Link>
+        <Link to="/find-bike-path" className="home-page__link-item">
+          <span>找路線</span>
+        </Link>
+        <Link to="/find-food-and-tour" className="home-page__link-item">
+          <span>找美食 / 景點</span>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
