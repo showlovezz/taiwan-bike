6fe546c8ef5643a615a2c7d62d58d24de6577079
vita ora <vitata710@gmail.com>
2021-11-19 08:57:41 +0800

    apply nav menu of rwd

--- src/components/MenuNavbar/index.js
+++ src/components/MenuNavbar/index.js
@@ -1,40 +1,53 @@
-import React from 'react';
-import { Button, Container, Form, FormControl, Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap';
-import { useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
+import { ReactComponent as CloseMenu } from '../../assets/images/close.svg';
+import { ReactComponent as MenuIcon } from '../../assets/images/menu.svg';
+import './styles.scss';
 
 const MenuNavbar = () => {
+  const [click, setClick] = useState(false);
   const location = useLocation();
 
   if (location.pathname === '/') return <></>;
 
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
-    <Navbar bg="light" expand={false}>
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-        <Navbar.Toggle aria-controls="offcanvasNavbar" />
-        <Navbar.Offcanvas id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" placement="end">
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form className="d-flex">
-              <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
+    <nav className="custom-navbar">
+      <Container className="custom-navbar__block">
+        <div className="custom-navbar__brand">
+          <Link to="/">
+            <span>YuoBike</span>
+          </Link>
+        </div>
+        <ul className={click ? 'custom-navbar__menu active' : 'custom-navbar__menu'}>
+          <li onClick={closeMobileMenu} className="custom-navbar__menu-item">
+            <Link to="/find-bikes">
+              <span>找 YouBike</span>
+            </Link>
+          </li>
+          <li onClick={closeMobileMenu} className="custom-navbar__menu-item">
+            <Link to="/find-bike-path">
+              <span>找路線</span>
+            </Link>
+          </li>
+          <li onClick={closeMobileMenu} className="custom-navbar__menu-item">
+            <Link to="/find-food-and-tour">
+              <span>找美食 / 景點</span>
+            </Link>
+          </li>
+        </ul>
+        <div className="custom-navbar__menubar" onClick={handleClick}>
+          {click ? (
+            <CloseMenu className="custom-navbar__menubar-icon" />
+          ) : (
+            <MenuIcon className="custom-navbar__menubar-icon" />
+          )}
+        </div>
       </Container>
-    </Navbar>
+    </nav>
   );
 };
 
